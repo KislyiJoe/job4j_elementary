296e6f6d00b45fa0d502a67aea1f7fea5354570a
KislyiJoe <kislyijoe@ofmd.ru>
2023-09-24 19:27:48 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Danil Kislyakov");
+        System.out.println("04.06.2002");
+    }
+}
