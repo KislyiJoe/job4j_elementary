4b683bdb55bdb1a4eb9fd659da274aeec2d06024
KislyiJoe <kislyijoe@ofmd.ru>
2023-09-24 19:38:55 +0400

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/ru/job4j/info.java
+++ src/main/java/ru/job4j/ru/job4j/info.java
@@ -2,6 +2,6 @@ package ru.job4j.ru.job4j;
 
 public class info {
     public static void main(String[] args) {
-        System.out.println("24.09.2023");
+         System.out.println("24.09.2023");
     }
 }
